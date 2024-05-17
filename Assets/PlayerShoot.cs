4f0b4b035e17d423c533fcf3e46aa3b0ff144fde
Amro Belbeisi <abelbeisi@conestogac.on.ca>
2024-05-17 14:05:57 -0400

    Initial Commit

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    private Camera fpscam;
+    // Start is called before the first frame update
+    void Start()
+    {
+       fpscam = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 rayOrigin = fpscam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(rayOrigin, fpscam.transform.forward, out hit))
+            {
+                Debug.Log("Hit: " + hit.normal);
+            }
+        }
+    }
+}
