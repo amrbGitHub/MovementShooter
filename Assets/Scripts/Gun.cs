ade581a94a0f52dcd9fe9100f0cc41f2075c266f
Amro Belbeisi <amrbelbeisi002@gmail.com>
2024-07-21 16:41:41 -0400

    Reworking almost everything, shooting, movement etc.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem shootingParticleSystem;
+    [SerializeField]
+    private Transform bulletSpawnPoint;
+    [SerializeField]
+    private GameObject bulletHole;
+    [SerializeField]
+    private TrailRenderer bulletTrail;
+    [SerializeField]
+    private float shotDelay = 0.5f;
+    [SerializeField]
+    private LayerMask playerMask;
+
+    private float lastShootTime;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        if (lastShootTime + shotDelay < Time.time)
+        {
+            shootingParticleSystem.Play();
+            Vector3 direction = GetDirection();
+
+            if (Physics.Raycast(bulletSpawnPoint.position, direction, out RaycastHit hit,float.MaxValue,playerMask))
+            {
+                TrailRenderer trail = Instantiate(bulletTrail, bulletSpawnPoint.position,Quaternion.identity);
+                StartCoroutine(SpawnTrail(trail, hit));
+
+                lastShootTime = Time.time;
+            }
+        }
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector3 direction = transform.forward;
+
+        direction.Normalize();
+
+        return direction;
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer trail, RaycastHit hit)
+    {
+        float time = 0;
+        Vector3 startPosition = trail.transform.position;
+
+        while(time < 1)
+        {
+            trail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
+            time += Time.deltaTime / trail.time;
+
+            yield return null;
+        }
+        trail.transform.position = hit.point;
+        Destroy(trail.gameObject, trail.time);
+    }
+    
+}
